96d9c8b1745d1cc3c0fabdb0dadf2ac72a849104
Ahbab <ahbabshahin7788@gmail.com>
2023-06-10 09:20:31 +0600

    package json edited

--- server-side/db/connect.js
+++ server-side/db/connect.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-
+// mongoose.set('strictQuery', true);
 const connectDB = (url) => {
-  return mongoose.connect(url);
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	});
 };
 
 module.exports = connectDB;
